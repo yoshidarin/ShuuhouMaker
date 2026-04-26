c8653e859e3b10789e7d53830caecabb0dc5d905
riny <108649619+rinybox@users.noreply.github.com>
2026-04-26 19:35:26 +0900

    不要な引数/クラス/変数の削除・パッケージ分離

--- src/main/java/com/example/shuuhoumaker/provider/TemplateProvider.java
+++ src/main/java/com/example/shuuhoumaker/provider/TemplateProvider.java
@@ -1,13 +1,10 @@
 package src.main.java.com.example.shuuhoumaker.provider;
 
-import src.main.java.com.example.shuuhoumaker.model.data.DayData;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.text.MessageFormat;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 
